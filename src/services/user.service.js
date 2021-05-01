09a354c073b05e5d8e26fcb0e9c60b4f99f616fb
lexx54 <chapelexx54@gmail.com>
2021-05-01 15:21:23 -0400

    updated endpoint

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import authHeader from "./auth-header";
 
 // const API_URL = "http://localhost:8080/api/test/";
-const API_URL = "https://obscure-river-43181.herokuapp.com/api/test"
+const API_URL = "https://obscure-river-43181.herokuapp.com/api/test/"
 
 const getPublicContent = () => {
   return axios.get(API_URL + "all");
